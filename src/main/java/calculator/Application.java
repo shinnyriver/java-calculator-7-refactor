2bb18c41c06e449f5ea62ac139d6c3fdffc00b2e
Changsu Park <gd12091@gmail.com>
2024-10-21 21:52:00 +0900

    feat: controller와 유저 연결부 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,9 +1,14 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.Console;
+import calculator.controller.CalculatorController;
+import calculator.view.InputView;
+import calculator.view.OutputView;
 
 public class Application {
-    public static void main(String[] args){
-        //TODO
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        CalculatorController controller = new CalculatorController(inputView, outputView);
+        controller.run();
     }
 }
